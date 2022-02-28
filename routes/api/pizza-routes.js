66334670d115ed88bc3e139ecb08e336aec3eeb7
emilypier <piersoee@miamioh.edu>
2022-02-27 20:13:48 -0500

    add controller methods in api/pizza-routes.js

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {     // destructuring the method names out of the imported object
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
+} = require('../../controllers/pizza-controller');
+
+// Set up GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+  
+module.exports = router;
