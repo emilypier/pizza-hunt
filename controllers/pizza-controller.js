fa0fa756172424158b8b99dccd571ecb6f0ebf53
emilypier <piersoee@miamioh.edu>
2022-03-02 15:00:01 -0500

    addded validation to pizza model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -50,16 +50,16 @@ const pizzaController = {
   // update pizza by id. PUT /api/pizzas/:id
   updatePizza({ params, body }, res) {
     //{ new: true } means Mongoose will return the new version of document. 
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.status(400).json(err));
-  },
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.status(400).json(err));
+},
 
   // delete pizza. DELETE /api/pizzas/:id
   // will find the document to be returned and also delete it from db
